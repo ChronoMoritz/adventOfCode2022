7f0148bcf015edf52e80c6256be5e42b348d63ad
Chrono_Moritz <moritz@christl-hamburg.de>
2022-12-01 00:19:39 +0100

    init

---
+++ aoc01.py
@@ -0,0 +1,5 @@
+file1 = open('./aoc01data.txt', 'r')
+Lines = file1.readlines()
+for line in Lines:
+    #stuff
+    print(line)
---
+++ template.py
@@ -0,0 +1,5 @@
+file1 = open('./aoc0Xdata.txt', 'r')
+Lines = file1.readlines()
+for line in Lines:
+    #stuff
+    print(line)
