5d2d93c5ce6a842070dc4061f93aeb24c5c74647
Chrono_Moritz <moritz@christl-hamburg.de>
2022-12-01 10:25:04 +0100

    day1

--- aoc01.py
+++ aoc01.py
@@ -1,5 +1,27 @@
 file1 = open('./aoc01data.txt', 'r')
 Lines = file1.readlines()
+maxElve = 0
+max2Elve = 0
+max3Elve = 0
+tmp = 0
 for line in Lines:
-    #stuff
-    print(line)
+    if line != "\n":
+        tmp = tmp + int(line)
+    else:
+        if tmp > max3Elve:
+            if(tmp > max2Elve):
+                if(tmp > maxElve):
+                    max3Elve = max2Elve
+                    max2Elve = maxElve
+                    maxElve = tmp
+                else:
+                    max3Elve = max2Elve
+                    max2Elve = tmp
+            else:
+                print("here with:", tmp)
+                max3Elve = tmp
+        tmp = 0
+
+print(maxElve, max2Elve, max3Elve)
+print("CombinedElves: ", maxElve + max2Elve + max3Elve)
+print("maximum Elve:", maxElve)
